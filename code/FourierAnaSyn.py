676d1b9eb199d7fc2cc8ed8821ad6a52a2af9a86
Spiegeldondi <dominik.freinberger@gmail.com>
2020-05-28 21:14:02 +0200

    sync to Laptop

--- code/FourierAnaSyn.py
+++ code/FourierAnaSyn.py
@@ -4,6 +4,25 @@ import matplotlib.pyplot as plt
 from numpy.fft import fft, fftfreq
 
 #%%
+path = '/home/domi/Dokumente/SchroedingerByML/potentials/A_original_potentials/'
+
+#%%
+bins = 128
+seedmax = 20
+trainy = []
+validy = []
+
+for i in range(seedmax):
+    with open(path+'test_out/test_out'+str(i)+'.csv', 'r') as csvfile:
+        flurg = csv.reader(csvfile)
+        for row in flurg:
+            trainy.append([float(num) for num in row])
+    with open(path+'valid_out/valid_out'+str(i)+'.csv', 'r') as csvfile:
+        flurg = csv.reader(csvfile)
+        for row in flurg:
+            validy.append([float(num) for num in row])
+
+#%%
 def cosShift(re, im):
     shift = np.arctan2(im, re) * 180/np.pi
     shift = np.floor(shift)
@@ -11,161 +30,76 @@ def cosShift(re, im):
         shift += 360
     return shift
 
-#%%
-index = 322
+# interpolate wavefunction for more precise FFT
+index = np.random.randint(0, 9600)
 x = np.linspace(0, 126, 127)
 y = trainy[index]
 xvals = np.linspace(0, 126, 1000)
 yinterp = np.interp(xvals, x, y)
 
-# plt.subplot(211)
-# plt.plot(y)
-# plt.subplot(212)
-# plt.plot(yinterp)
-
 #%%
-# period and frequency
-T = 1
+# spatial resolution and domain
+T = 127
 v = 1/T
-
 # angular frequency
 w = 2*np.pi*v
-
 # spatial resolution and domain
 n = 2000
 x = np.linspace(0, T, n)
+xsynth = np.linspace(0, T, n)
 
-xsynth = np.linspace(0, 2000, n)
-
-# parameters of periodic function
-A1 = 12
-f1 = 0.5
-phi1 = 0
-d1 = 0
-
-A2 = 6
-f2 = 2
-phi2 = 0
-d2 = 0
-
-# signal components
-sig1 = A1 * np.sin(f1 * w * x + phi1) + d1
-sig2 = A2 * np.sin(f2 * w * x + phi2) + d2
-sig3 = 2 * np.sin(8*w*x)
-
-# signal assambly
-sgnl = sig1 + sig2 + sig3
-sgnl = np.append(sgnl, -sgnl[::-1])
+# extend and mirror signal for Half Range Fourier Series
 sgnl = np.append(yinterp, -yinterp[::-1])
+
 # frequency domain
 freqs = fftfreq(n) * n
 
 # true physical frequency domain
 realFreqs = freqs > 0
 
-# fft
+# FFT
 fft_vals = fft(sgnl)
 
-# true physical fft
+# true physical FFT
 fft_phys = 2.0*np.abs(fft_vals/n)
 
-#%%
+# compose signal out of sinusoidal paramteres
 sgnl_synth = 0
-for f in range(1, 200):
+for f in range(1, 1000):
     Asynth = fft_phys[f]
     re = fft_vals.real[f]
     im = fft_vals.imag[f]
     s =  cosShift(re, im)*np.pi/180
     sgnl_synth += Asynth * np.cos(f * w * x + s)
 
-#%%  
-f1 = 1
-f2 = 2
-f3 = 3
-f4 = 4
-f5 = 5
-f6 = 6
-f7 = 7
+# plot analyzed and synthized wavefunctions
+plt.plot(sgnl[0:1000], linewidth=6)
+plt.plot(sgnl_synth[0:1000], linewidth=6)
+plt.tight_layout()
 
-Asynth1 = fft_phys[f1]
-Asynth2 = fft_phys[f2]
-Asynth3 = fft_phys[f3]
-Asynth4 = fft_phys[f4]
-Asynth5 = fft_phys[f5]
-Asynth6 = fft_phys[f6]
-Asynth7 = fft_phys[f7]
-
-def cosShift(re, im):
-    shift = np.arctan2(im, re) * 180/np.pi
-    shift = np.floor(shift)
-    if shift < 0:
-        shift += 360
-    return shift
-
-re1 = fft_vals.real[f1]
-im1 = fft_vals.imag[f1]
-
-re2 = fft_vals.real[f2]
-im2 = fft_vals.imag[f2]
-
-re3 = fft_vals.real[f3]
-im3 = fft_vals.imag[f3]
-
-re4 = fft_vals.real[f4]
-im4 = fft_vals.imag[f4]
-
-re5 = fft_vals.real[f5]
-im5 = fft_vals.imag[f5]
-
-re6 = fft_vals.real[f6]
-im6 = fft_vals.imag[f6]
-
-re7 = fft_vals.real[f7]
-im7 = fft_vals.imag[f7]
-
-s1 = cosShift(re1, im1)*np.pi/180
-s2 = cosShift(re2, im2)*np.pi/180
-s3 = cosShift(re3, im3)*np.pi/180
-s4 = cosShift(re4, im4)*np.pi/180
-s5 = cosShift(re5, im5)*np.pi/180
-s6 = cosShift(re6, im6)*np.pi/180
-s7 = cosShift(re7, im7)*np.pi/180
-
-
-#%%
-sgnl_synth = Asynth1 * np.cos(f1 * w * x + s1) + Asynth2 * np.cos(f2 * w * x + s2) + Asynth3 * np.cos(f3 * w * x + s3)
-sgnl_synth = sgnl_synth + Asynth4 * np.cos(f4 * w * x + s4) + Asynth5 * np.cos(f5 * w * x + s5) + Asynth6 * np.cos(f6 * w * x + s6)
-sgnl_synth = sgnl_synth + Asynth7 * np.cos(f7 * w * x + s7)
-
-#%%
-plt.plot(sgnl[0:1000])
-plt.plot(sgnl_synth[0:1000])
-
-#%%
-plt.figure(1)
-
+# plot all information
+plt.figure(2)
 plt.subplot(511)
 plt.grid(1)
-#plt.title('original Signal')
+plt.title('Signal extended as odd function')
 plt.plot(sgnl)
 
 plt.subplot(512)
 plt.grid(1)
-#plt.title('double inverted Signal')
-#plt.plot(sgnl)
+plt.title('Synthized Signal')
 plt.plot(xsynth, sgnl_synth)
 
 plt.subplot(513)
 plt.grid(1)
-#plt.title('Real Part')
+plt.title('Real Part')
 plt.plot(fft_vals.real, '.')
 
 plt.subplot(514)
 plt.grid(1)
-#plt.title('Imag Part')
+plt.title('Imaginary Part')
 plt.plot(fft_vals.imag, '.')
 
 plt.subplot(515)
 plt.grid(1)
-#plt.title('Absolute Value')
+plt.title('Processed Amplitudes (processed absolute values)')
 plt.plot(fft_phys, '.')
